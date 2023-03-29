3eb8e6102165cd0bd593a5f89788807039df5c2e
Jimna Johny <jimnajohny@gmail.com>
2023-03-28 22:11:56 -0400

    Modifying my second Java commit

--- src/trial1Package/Trial1Class.java
+++ src/trial1Package/Trial1Class.java
@@ -3,7 +3,7 @@ package trial1Package;
 public class Trial1Class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("My name is Jimna Johny");
 
 	}
 
